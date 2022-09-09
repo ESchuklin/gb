3230ab9862b6bac7ee8c8a7c77f6ee642a27f646
ESchuklin <sharprazor@mail.ru>
2022-09-09 14:24:13 +0400

    v4

---
+++ dz3.py
@@ -0,0 +1,18 @@
+#1
+# def my_numbers():
+#     try:
+#         num1 = int(input('введите первое число: '))
+#         num2 = int(input('введите второе число: '))
+#         d = num1 / num2
+#         print(d)
+#     except:
+#         print('На ноль делить нельзя, введите целое число')
+# my_numbers()
+
+#2
+def people(name, surname, year, city, email, tel_num):
+    print(f"Имя = {name}, Фамилия = {surname}, Год = {year}, Город = {city}, Эл.почта = {email}, Тел.номер = {tel_num}")
+people('Иван', 'Петров', '1965', 'Москва', 'ivan@mail.ml', '89073451298')
+#people(name = input('Введите имя: '), surname = input('Введите фамилию: '), year = input('Введите год: '), city = input('Введите город: '), email = input('Введите email: '), tel_num = input('Введите телефонны номер: '))
+
+#3
